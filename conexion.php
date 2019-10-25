811939f0188441531f53b92def39d249499ad822
unknown <45887989+alematrix11@users.noreply.github.com>
2019-10-25 16:27:45 -0300

    Se agrego nuevas query's para llamar a las nuevas consultas desde una funcion principal, y tambien con el uso de transaciones

--- conexion.php
+++ conexion.php
@@ -1,23 +1,58 @@
 
 <?php 
 
-    //Creamos una variable donde vamos a crear la conexion
-    //Se instancia con mysqli
-
-    //1_ se agrega el host del servidor
-    //2_ se agrega el usuario del servidor
-    //3_ se agrega la contraseña del usuario del servidor
-    //4_ se agrega se agrega el nombre de la base de datos
-    $conex = new mysqli("localhost", "root", "", "usuarios");
-    
-    if($conex){
-        echo '<br>La conexion se realizo con exito';
-    }
-
-    //Se verifica que la conexion esta funcionando
-    if (mysqli_connect_errno()){
+    //Se crea una funcion, a la que se le debe pasar una query, lo que sera la consulta que se hace a la base de datos
+    function consultarSQL($query){
         
-        //En caso de que la conexion no funciona, muestra que error tiene la conexion    
-        printf("Conexion fallida", mysqli_connect_error());
-    
+        //Creamos una variable donde vamos a crear la conexion
+        //Se instancia con mysqli
+
+        //1_ se agrega el host del servidor
+        //2_ se agrega el usuario del servidor
+        //3_ se agrega la contraseña del usuario del servidor
+        //4_ se agrega se agrega el nombre de la base de datos
+        $conex = new mysqli("localhost", "root", "", "usuarios");
+
+        if($conex){
+            echo '<br>La conexion se realizo con exito';
+        }
+
+        //Se verifica que la conexion esta funcionando
+        if (mysqli_connect_errno()){
+
+            //En caso de que la conexion no funciona, muestra que error tiene la conexion    
+            printf("Conexion fallida", mysqli_connect_error());
+
+        }
+        
+        $conex-> set_charset("utf-8");
+        $conex-> autocommit(FALSE);
+        $conex-> begin_transaction(MYSQLI_TRANS_START_WITH_CONSISTENT_SNAPSHOT);
+        
+        //Se establece un condicional para cuando la consulta se realiza correctamente
+        //Se establece la consulta y se guarda en una variable consulta
+        if($consulta = $conex->query($query)){
+            
+            //Se llama al metodo commit
+            if($conex->commit()){
+            
+                echo '<br>Datos guardados';
+            
+            }else{
+            
+                echo '<br>Error datos no guardados';
+            
+            }
+           
+        }else{
+            
+            //Se llama al metodo commit
+            $conex->rollback();
+            
+                echo '<br>Error datos no guardados';
+            
+        }
+           
+        return $consulta;
+           
     }
--- index.php
+++ index.php
@@ -6,7 +6,25 @@
     
     //LLamamos a la variable que le pasamos las variable de la conexion
     //Se establece una consulta SQL, para agregar registros a la tabla
-    $conex->query("INSERT INTO registro(Nombre, Apellido, Email, Clave) 
-    VALUES('PrimerNombre', 'SegundoNombre', 'PrimerEmail@gmail.com', 'PrimerClave')");
+    //$conex->query("INSERT INTO registro(Nombre, Apellido, Email, Clave) 
+    //VALUES('PrimerNombre', 'PrimerNombre', 'PrimerEmail@gmail.com', 'PrimerClave')");
+
+    $consulta_datos = ("INSERT INTO registro(Nombre, Apellido, Email, Clave) 
+    VALUES('PrimerNombre', 'PrimerApellido', 'PrimerEmail@gmail.com', 'PrimerClave')");
+
+    $consulta_datos_2 = ("INSERT INTO registro(Nombre, Apellido, Email, Clave) 
+    VALUES('SegundoNombre', 'SegundoApellido', 'SegundoEmail@gmail.com', 'SegundoClave')");
+
+    $consulta_datos_3 = ("INSERT INTO registro(Nombre, Apellido, Email, Clave) 
+    VALUES('TercerNombre', 'TercerApellido', 'TercerEmail@gmail.com', 'TercerClave')");
+
+    $consulta_datos_4 = ("INSERT INTO registro(Nombre, Apellido, Email, Clave) 
+    VALUES('CuartoNombre', 'CuartoApellido', 'CuartoEmail@gmail.com', 'CuartoClave')");
+
+    //Utilizando la funcion de conexion de puede llamar simplement con su nombre
+    consultarSQL($consulta_datos);
+    consultarSQL($consulta_datos_2);
+    consultarSQL($consulta_datos_3);
+    consultarSQL($consulta_datos_4);
 
 ?>
